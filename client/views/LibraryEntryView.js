5a72cd167027e803bc825c8b0e64c8fbc3d0f2cc
mybrainishuge <richardamay@gmail.com>
2016-05-04 22:21:42 -0700

    Remove comment because we understand all of the things

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -7,7 +7,6 @@ var LibraryEntryView = Backbone.View.extend({
 
   events: {
     'click': function() {
-      // why is .model required?
       this.model.play();
       this.model.enqueue();
     }
